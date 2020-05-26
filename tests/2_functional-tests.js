c5082ad7bf28d1be6284a9491c5942c46d055f76
Glitch (fcc-stock-price-checker-liu) <none>
2020-05-26 09:14:04 +0000

    complete testcase 1-3

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -32,15 +32,17 @@ suite('Functional Tests', function() {
           assert.isNumber(res.body.stockData.price, 'should be a string');
           assert.isNumber(res.body.stockData.likes, 'should be a number');
           assert.equal(res.body.stockData.stock, 'GOOG');
-          assert.equal(res.body.stockData.likes, 0); 
+          assert.equal(res.body.stockData.likes, 2); 
           done();
         });
       });
       
-      test.only('1 stock with like', function(done) {
+      test('1 stock with like', function(done) {
+        this.timeout(3000);
         chai.request(server)
           .get('/api/stock-prices')
           .query( {stock: 'goog', like: 'true'} )
+          // .get('/api/stock-prices?stock=goog&like=true')
           .end(function(err, res) {
             assert.equal(res.status, 200);
             assert.isObject(res.body, 'should be an object');
@@ -52,14 +54,31 @@ suite('Functional Tests', function() {
             assert.isNumber(res.body.stockData.price, 'should be a string');
             assert.isNumber(res.body.stockData.likes, 'should be a number');
             assert.equal(res.body.stockData.stock, 'GOOG');
-            // assert.equal(res.body.stockData.likes, 1);
+            assert.equal(res.body.stockData.likes, 2); //must delete {ip: '127.0.0.1'} before running this test
             done();
         })
         
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query( {stock: 'goog', like: 'true'} )
+          // .get('/api/stock-prices?stock=goog&like=true')
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isObject(res.body, 'should be an object');
+            assert.property(res.body, 'stockData', 'should have stockData')
+            assert.property(res.body.stockData, 'stock', 'should have stock');
+            assert.property(res.body.stockData, 'price', 'should have price');
+            assert.property(res.body.stockData, 'likes', 'should have likes');
+            assert.isString(res.body.stockData.stock, 'should be a string');
+            assert.isNumber(res.body.stockData.price, 'should be a string');
+            assert.isNumber(res.body.stockData.likes, 'should be a number');
+            assert.equal(res.body.stockData.stock, 'GOOG');
+            assert.equal(res.body.stockData.likes, 3);
+            done();
+        })
       });
       
       test('2 stocks', function(done) {
